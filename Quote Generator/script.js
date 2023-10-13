84b704914e6989b8b6e02e93fd222619bb2bf9df
Riya Ranjan Jha <112813772+riya-rjha@users.noreply.github.com>
2023-10-13 10:14:27 +0530

    Add files via upload

---
+++ Quote Generator/script.js
@@ -0,0 +1,12 @@
+const api_url = "https://api.quotable.io/random";
+const quoteRandom = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const getQuote = async(url)=>{
+    const response = await fetch(url);
+    var quote = await response.json();
+    quoteRandom.innerHTML = quote.content;
+    quoteAuthor.innerHTML = quote.author;
+    
+}
+
+getQuote(api_url);
