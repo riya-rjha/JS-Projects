75338fa6f89cff83da9bf62b87ab7db692077916
Riya Ranjan Jha <112813772+riya-rjha@users.noreply.github.com>
2023-10-22 19:47:02 +0530

    Add files via upload

---
+++ Notes app/script.js
@@ -0,0 +1,40 @@
+let notesContainer = document.querySelector('.notes-container');
+let btn = document.getElementById('btn');
+//notes + inputBox
+let inputText = document.querySelectorAll('.notes-content');
+
+btn.addEventListener('click', ()=>{
+    let inputBox = document.createElement('p');
+    inputBox.className = 'notes-content';
+    inputBox.setAttribute('contenteditable', 'true');
+    let imgDlt = document.createElement('img');
+    imgDlt.src = 'Images/delete.png';
+    notesContainer.appendChild(inputBox).appendChild(imgDlt);
+})
+
+notesContainer.addEventListener('click',(event)=>{
+    if(event.target.tagName === 'IMG'){
+        event.target.parentElement.remove();
+        saveData();
+    }
+    else if (event.target.tagName === 'p'){
+        inputText = document.querySelectorAll(".notes-content");
+        inputText.forEach(i=>{
+            i.onkeyup = () => {
+                saveData();
+            }
+        })
+    }
+})
+
+//Set items to storage
+function saveData() {
+    localStorage.setItem('inputBox', notesContainer.innerHTML);
+}
+
+//Get items from storage
+function getItemTask() {
+    notesContainer.innerHTML = localStorage.getItem('inputBox');
+}
+
+getItemTask(); 
