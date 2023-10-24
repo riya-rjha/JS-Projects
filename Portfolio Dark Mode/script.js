e2683ce6fb672d88c3cd844e050562aa22025a8c
Riya Ranjan Jha <112813772+riya-rjha@users.noreply.github.com>
2023-10-24 11:11:52 +0530

    Resolving Syntax Error

--- Portfolio Dark Mode/script.js
+++ Portfolio Dark Mode/script.js
@@ -1,7 +1,7 @@
 var img = document.getElementById('icon');
 img.addEventListener('click', ()=>{
     document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains = 'dark-mode'){
+    if (document.body.classList.contains ('dark-mode')){
         img.src = 'sun.png';
     }
     else{
