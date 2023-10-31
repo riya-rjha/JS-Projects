426bd7e487612a562d2a9ad1c3ff4462f8452bcf
Riya Ranjan Jha <112813772+riya-rjha@users.noreply.github.com>
2023-11-01 00:20:12 +0530

    Adding sound effect to all btns

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,53 +2,54 @@ let display = document.getElementById('input');
 let effect = new Audio("Audio/Pop.mp3");
 let msg = "";
 //All numerics & symbols
-const buttonClick = (value) =>{
-    msg += value; 
+const buttonClick = (value) => {
+    msg += value;
     display.value = msg;
     effect.play();
 }
 
+
 //AC - Clear all
 let reset = document.getElementById('reset');
-reset.addEventListener('click', ()=>{
+reset.addEventListener('click', () => {
     display.value = '';
-    msg='';
     effect.play();
 })
 
 //DE - Delete last element
 let del = document.getElementById('del');
-del.addEventListener('click', ()=>{
+del.addEventListener('click', () => {
     msg = msg.slice(0, -1); // Remove the last character from msg
     display.value = msg;
     effect.play();
 })
 
 let btn = document.getElementById('equalSign');
-btn.addEventListener('click', newFunc = () =>{
-    //created a function evaluate()
-    //instead of using eval(display.value)
-     evaluate();
+btn.addEventListener('click', newFunc = () => {
+    evaluate(); //instead of eval
     setInterval(() => {
-        if(display.value === ''){
+        if (display.value === '') {
             location.reload();
         }
     }, 5000);
-   effect.play();
+    effect.play();
+
 })
 
-display.addEventListener('keypress', (event)=>{
-    if(event.key === 'Enter'){
+display.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
         event.preventDefault();
         newFunc();
     }
+    
+    effect.play();
 })
 
-const evaluate = () =>{
+const evaluate = () => {
     let expression = display.value;
     let nos;
     let ans = 0;
-    for(let i=0; i<expression.length; i++){
+    for (let i = 0; i < expression.length; i++) {
         switch (expression[i]) {
             case '+':
                 nos = expression.split('+');
@@ -64,13 +65,14 @@ const evaluate = () =>{
                 nos = expression.split('-');
                 //convert string to numeric values
                 ans += parseFloat(nos[0]) - parseFloat(nos[1]);
+                effect.play();
                 break;
             case '/':
                 nos = expression.split('/');
                 //convert string to numeric values
                 ans += parseFloat(nos[0]) / parseFloat(nos[1]);
                 break;
-        
+
             default:
                 break;
         }
