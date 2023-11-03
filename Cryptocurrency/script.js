0786e6589d947d8a10a452d56ae4734c72525430
Riya Ranjan Jha <112813772+riya-rjha@users.noreply.github.com>
2023-11-03 10:38:37 +0530

    Using async & await

--- Cryptocurrency/script.js
+++ Cryptocurrency/script.js
@@ -6,16 +6,30 @@ let bitcoin = document.getElementById('bitcoin');
 let dogecoin = document.getElementById('dogecoin');
 let ethereum = document.getElementById('ethereum');
 
-var setting = {
-    "async" : true, 
-    "scrossDomain" : true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
-    "method" : "GET", 
-    "headers" : {}
-}
+// var setting = {
+//     "async" : true, 
+//     "scrossDomain" : true,
+//     "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+//     "method" : "GET", 
+//     "headers" : {}
+// }
 
-$.ajax(setting).done(function (response) {
+// $.ajax(setting).done(function (response) {
+//     bitcoin.innerHTML = response.bitcoin.usd;
+//     dogecoin.innerHTML = response.dogecoin.usd;
+//     ethereum.innerHTML = response.ethereum.usd;
+// })
+
+const apiURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd";
+
+//Using async & await 
+
+async function getCost(url) {
+    let data = await fetch(url);
+    let response = await data.json();
     bitcoin.innerHTML = response.bitcoin.usd;
     dogecoin.innerHTML = response.dogecoin.usd;
     ethereum.innerHTML = response.ethereum.usd;
-})
+}
+
+getCost(apiURL);
