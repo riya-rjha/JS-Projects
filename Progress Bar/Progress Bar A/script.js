51442e8a8562bea389558748b6d3a46ca6c7adcf
riya-rjha <riyaranjanjha2006@gmail.com>
2024-03-17 12:50:10 +0530

    Updating Progress Bar

--- Progress Bar/Progress Bar A/script.js
+++ Progress Bar/Progress Bar A/script.js
@@ -3,22 +3,21 @@ const progressBar = document.getElementById('progressBar');
 const gradientEnd = document.getElementById('gradientEnd');
 
 let progress = 0;
-const updateInterval = 50; // Milliseconds between updates
+const updateInterval = 50; 
 const maxProgress = 65;
 
 function updateProgress() {
     progress++;
     if (progress > maxProgress) {
         clearInterval(interval);
-        progress = maxProgress; // Set progress to exactly 65
+        progress = maxProgress;
     }
     progressText.textContent = progress + '%';
     progressBar.style.strokeDashoffset = 520 - (520 * progress) / 100;
 
-    // Calculate gradient transition
-    const gradientProgress = progress / maxProgress; // Progress normalized to 0-1 range
+    const gradientProgress = progress / maxProgress; 
     gradientEnd.setAttribute('offset', `${gradientProgress * 100}%`);
 }
 
-updateProgress(); // Initial update
+updateProgress(); 
 const interval = setInterval(updateProgress, updateInterval);
